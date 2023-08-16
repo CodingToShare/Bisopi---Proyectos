848f1e4a430d97a9e975078e7ac093dc65f26a31
JustSharingSomeCode <62771039+JustSharingSomeCode@users.noreply.github.com>
2023-08-15 23:55:50 -0500

    Add Groups

---
+++ Bisopi - Proyectos/Models/Group.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bisopi___Proyectos.Models
+{
+    public class Group
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; } = string.Empty;
+    }
+}
